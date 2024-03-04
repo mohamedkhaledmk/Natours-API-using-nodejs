ac68bba4419fa7c8dae2fdf197f596d125a34d0e
Mohamed Khaled <mohamed.kmohamed@miu-net.local>
2024-03-04 16:35:16 +0200

    limit the coming requests

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 const exp = require('constants');
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
+const helmet = require('helmet');
+
 const app = express();
+
+//set security HTTP headers
+app.use(helmet());
+
+//limit requests
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 //1) MIDDLEWARES
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization
+
+
+
+
 app.use(express.static(`${__dirname}/public`));
 //this middleware will be executed eachtime there is a new req. ---- if i removed the next(), it will not proceed to the following middleware function in the middleware stack and it will die in this middleware func
+// Test middleware
 app.use((req, res, next) => {
   console.log('Hello from my middleware 👋');
   req.requestTime = new Date().toISOString(); //to add the req time to the req body
